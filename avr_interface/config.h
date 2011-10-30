3e47d3a3e6343fc0dde8bb458fc7527a7c18af8e
Hans-Gert Dahmen <sexyludernatascha@gmail.com>
2011-10-30 00:24:13 +0000

    normalizing repository

---
+++ avr_interface/config.h
@@ -0,0 +1,31 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+
+// SPI
+
+
+// SS Pin muss Ausgang sein wenn nicht als SS verwendet
+#define MCP_CS        PB4
+#define MCP_CMD_PORT  PORTB
+
+#define SPI_REG_PIN_MCP_INT  PIND
+#define SPI_PIN_MCP_INT      PD2
+//#define CAN_INTERRUPT
+
+
+#define F_MCP F_CPU
+
+
+
+// TWI_ADDRESS
+#include "../include/PowerCommander.h"
+
+
+#define LED_GRUEN   _BV(PA2)
+#define LED_ROT _BV(PA3)
+#define LED_BLAU  _BV(PA4)
+
+
+
+#endif // ifndef CONFIG_H
